ad6afc87316a81fad91a04721a6aa929de4e1583
Pablo <pablocortes1101@gmail.com>
2024-09-30 22:48:29 -0300

    feat: actualizar codigo de usersRoute. API funcional con dummy data

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -9,68 +9,84 @@ const users = [
     name: "tec_name A",
     rut: "11111",
     email: "company1@gmail.com",
-    pasword: "tec1",
+    password: "tec1",
   },
   {
     id: 2,
     name: "tec_name b",
     rut: "22222",
     email: "company2@gmail.com",
-    pasword: "tec2",
+    password: "tec2",
   },
 ];
+// Campos requeridos para métodos POST y PUT
+const requiredFields = ["name", "rut", "email", "password"];
 
 // Obtener users
 usersRouter.get("/", (req, res) => {
-  res.status(200).json(users);
+  return res.status(200).send(users);
 });
 
 // Buscar user por id
 usersRouter.get("/:id", (req, res) => {
   const user = users.find((c) => c.id === parseInt(req.params.id));
-
   if (!user) {
-    return res.status(404).send("User not found");
+    return res.status(404).send({ message: "user not found" });
   }
-  res.status(200).send(user);
+  return res.status(200).send(user);
 });
 
 // Crear user
 usersRouter.post("/", (req, res) => {
-  const new_user = {
-    id: users.length + 1,
-    name: req.body.name,
-    rut: req.body.rut,
-    email: req.body.email,
-    pasword: req.body.pasword,
-  };
-  users.push(new_user);
-  res.status(201).send(new_user);
+  try {
+    if (!requiredFields.every((field) => req.body[field])) {
+      return res.status(400).send({ message: "missing required fields" });
+    }
+    const newUser = {
+      id: users.length + 1,
+      name: req.body.name,
+      rut: req.body.rut,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    users.push(newUser);
+    return res.status(201).send({ message: "user created", user: newUser });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send({ message: err.message });
+  }
 });
 
 // Actualizar user
 usersRouter.put("/:id", (req, res) => {
-  const user = users.find((c) => c.id === parseInt(req.params.id));
-
-  if (!user) {
-    return res.status(404).send("User not found");
+  try {
+    if (!requiredFields.every((field) => req.body[field])) {
+      return res.status(400).send({ message: "missing required fields" });
+    }
+    const user = users.find((c) => c.id === parseInt(req.params.id));
+    if (!user) {
+      return res.status(404).send({ message: "user not found" });
+    }
+    user.name = req.body.name;
+    user.rut = req.body.rut;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    return res.status(200).send({ message: "user updated", user: user });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send({ message: err.message });
   }
-  user.name = req.body.name;
-  user.rut = req.body.rut;
-  user.email = req.body.email;
-  user.pasword = req.body.pasword;
-  res.status(200).send(user);
 });
 
 // Eliminar user
 usersRouter.delete("/:id", (req, res) => {
   const user = users.find((c) => c.id === parseInt(req.params.id));
   if (!user) {
-    return res.status(404).send("User not found");
+    return res.status(404).send({ message: "user not found" });
   }
   const index = users.indexOf(user);
   users.splice(index, 1);
-  res.status(200).send(user);
+  res.status(200).send({ message: "user deleted", user: user });
 });
 
 export default usersRouter;
