c2125234ad8402a0cb64e3d2cc38264690f6640a
Pablo <pablocortes1101@gmail.com>
2024-10-05 20:27:18 -0300

    fix: agregar UNIQUE a email y rut en tablas user y client

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -2,22 +2,21 @@ CREATE DATABASE sistema_tickets;
 
 USE sistema_tickets;
 
--- TODO: Agregar UNIQUE a email y rut
-CREATE TABLE
-    user (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        rut VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        password char(60) NOT NULL
-    );
+CREATE TABLE user
+(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    rut VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password CHAR(60) NOT NULL
+);
 
-CREATE TABLE
-    client (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        rut VARCHAR(255) NOT NULL,
-        companyRut VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        password char(60) NOT NULL
-    );
+CREATE TABLE client 
+(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    rut VARCHAR(255) NOT NULL UNIQUE,
+    companyRut VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password CHAR(60) NOT NULL
+);
