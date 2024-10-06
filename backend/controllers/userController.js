8e9ed098a795579409cdd00eb5b33fd6ba0b0253
Pablo <pablocortes1101@gmail.com>
2024-10-06 06:14:41 -0300

    feat: agregar rutas y funcionalidad para getUserById y createUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
 export const getUsers = async (req, res, next) => {
   try {
@@ -7,4 +8,42 @@ export const getUsers = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+};
+
+export const getUserById = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    return res.status(200).send(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createUser = async (req, res, next) => {
+  const { name, rut, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      rut,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).send({ message: "User created", user });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default {
+  getUsers,
+  getUserById,
+  createUser,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,59 +1,11 @@
 import express from "express";
-import { getUsers } from "../controllers/userController.js";
+import controller from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
-
-// // Campos requeridos para métodos POST y PUT
-// const requiredFields = ["name", "rut", "email", "password"];
-
-// // Obtener todos los usuarios
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const users = await userQueries.getUsers();
-//     return res.status(200).send(users);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// // Buscar usuario por id
-// router.get("/:id", async (req, res, next) => {
-//   try {
-//     const user = await userQueries.getUserById(req.params.id);
-
-//     if (!user) {
-//       return res.status(404).send({ message: "User not found" });
-//     }
-
-//     return res.status(200).send(user);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// // Crear usuario
-// router.post("/", async (req, res, next) => {
-//   // Verificar que se envíen todos los campos requeridos
-//   if (!requiredFields.every((field) => req.body[field])) {
-//     return res.status(400).send({ message: "Missing required fields" });
-//   }
-
-//   try {
-//     // Evitar que se asignen campos adicionales
-//     const user = await userQueries.createUser(
-//       req.body.name,
-//       req.body.rut,
-//       req.body.email,
-//       req.body.password
-//     );
-
-//     return res.status(201).send({ message: "User created", user: user });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+router.get("/", controller.getUsers);
+router.get("/:id", controller.getUserById);
+router.post("/", controller.createUser);
 
 // // Actualizar usuario
 // router.put("/:id", async (req, res, next) => {
