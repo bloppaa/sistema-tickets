3f4f206c331389600976f492511c2b857ddb1429
Pablo <pablocortes1101@gmail.com>
2024-10-06 04:39:53 -0300

    feat: agregar instancia de sequelize con mariadb

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mariadb",
+    port: process.env.DB_PORT,
+  }
+);
+
+export default sequelize;
