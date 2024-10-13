f7712b0651d24d7c1c1837da9b4f4e7b3c878d71
Pablo <pablocortes1101@gmail.com>
2024-10-13 16:58:19 -0300

    feat: agregar template de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>
-      App
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   )
 }
 
